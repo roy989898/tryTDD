1e1d3e7d1779833519a6f8c07b38500905693b9b
poming\pomingpo <roy989898@gmail.com>
2017-06-15 12:19:15 +0800

    add the Junit test for the LoginPresenter

---
+++ app/src/main/java/com/example/pomingpo/trytdd/Presenter/LoginPresenter.java
@@ -0,0 +1,23 @@
+package com.example.pomingpo.trytdd.Presenter;
+
+/**
+ * Created by pomingpo on 2017/6/15.
+ */
+
+public class LoginPresenter {
+    private int loginAttempt;
+    private static final int MAX_LOGIN_ATTEMPT = 3;
+
+    public int incrementLoginAttempt() {
+        loginAttempt++;
+        return loginAttempt;
+    }
+
+    public boolean isLoginAttempExceeded() {
+        return loginAttempt > MAX_LOGIN_ATTEMPT;
+    }
+
+    public boolean isLoginSuccess(String name, String password) {
+        return name.equals("roy") && password.equals("tdd");
+    }
+}
---
+++ app/src/test/java/com/example/pomingpo/trytdd/Presenter/LoginPresenterTest.java
@@ -0,0 +1,37 @@
+package com.example.pomingpo.trytdd.Presenter;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by pomingpo on 2017/6/15.
+ */
+public class LoginPresenterTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void checkIfLoginAttemptIsExceeded() {
+        LoginPresenter loginPresenter = new LoginPresenter();
+        Assert.assertEquals(1, loginPresenter.incrementLoginAttempt());
+        Assert.assertEquals(2, loginPresenter.incrementLoginAttempt());
+        Assert.assertEquals(3, loginPresenter.incrementLoginAttempt());
+        Assert.assertFalse(loginPresenter.isLoginAttempExceeded());
+    }
+
+    @Test
+    public void checkUsernameAndPasswordIsCorrect(){
+        LoginPresenter loginPresenter = new LoginPresenter();
+        Assert.assertTrue(loginPresenter.isLoginSuccess("roy","tdd"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
